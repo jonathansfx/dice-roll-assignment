93078bc26058543204acdf265fa3fa4456733816
jonathan <jonathanluo22@gmail.com>
2024-02-06 09:58:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Dice roll assignment
 import random
-n = 1
 loop = True
 while loop:
 # print menu
@@ -14,13 +13,45 @@ while loop:
 
 # selector
     if selection == "1":
-        print("Option 1: ")
+        print("\nRoll Dice Once")
+        randNum1 = random.randint(1,6)
+        randNum2 = random.randint(1,6)
+        sum = randNum1 + randNum2
+        print(f"{randNum1},{randNum2} (sum: {sum})")
     elif selection == "2":
-        print("Option 2:")
+        print("\nRoll Dice 5 Times")
+        n = 1
+        while n < 6:
+            randNum1 = random.randint(1,6)
+            randNum2 = random.randint(1,6)
+            sum = randNum1 + randNum2
+            print(f"{randNum1},{randNum2} (sum: {sum})")
+            n += 1
     elif selection == "3":
-        print("Option 3")
+        print("Roll Dice 'n' Times")
+        n = input("Enter how many times you would like to roll: ")
+        i = 0
+        while i < int(n):
+            randNum1 = random.randint(1,6)
+            randNum2 = random.randint(1,6)
+            sum = randNum1 + randNum2
+            print(f"{randNum1},{randNum2} (sum: {sum})")
+            i += 1
     elif selection == "4":
-        print("Option 4")
+        print("\nRoll Dice until Snake Eyes")
+        n = 0
+        loop = True
+        while loop:
+            randNum1 = random.randint(1,6)
+            randNum2 = random.randint(1,6)
+            sum = randNum1 + randNum2
+            n += 1
+            if sum == 2:
+                loop = False
+                print(f"SNAKE EYES!!!! it took you ${n} rolls")
+            elif:
+                loop = True
+
     elif selection == "5":
         print("EXIT")
         loop = False
